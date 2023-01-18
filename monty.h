66fa0eb3f28f62648aa580bf6ed5f66b4e175ee2
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-01-18 22:25:49 +0300

    fix

--- monty.h
+++ monty.h
@@ -18,11 +18,10 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -33,10 +32,9 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
 /* global variable */
 /**
  * struct glo_s - global variable
@@ -61,7 +59,7 @@ void integer_error(unsigned int c, stack_t *head);
 void pint_error(unsigned int c);
 void pop_error(stack_t **stack, unsigned int c);
 void add_error(unsigned int c);
-
+void check_errors(int argc, char *argv);
 /* More error functions */
 void sub_error(unsigned int c);
 void div_error(unsigned int c);
